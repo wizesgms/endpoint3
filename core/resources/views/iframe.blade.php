1777b64a875350de92d2a0ebb2513ade63fe390a
wizesgms <161136541+wizesgms@users.noreply.github.com>
2024-11-11 02:14:34 +0700

    Create iframe.blade.php

---
+++ core/resources/views/iframe.blade.php
@@ -0,0 +1,39 @@
+<html>
+
+<head>
+    <title>Pirate Gold Deluxe</title>
+    <style type="text/css">
+        html,
+        body,
+        iframe {
+            left: 0px;
+            top: 0px;
+            position: relative;
+            display: flex;
+            align-items: center;
+            background-color: black;
+            margin: 0px;
+            padding: 0px;
+            width: 100%;
+            height: 100%;
+            border: 0px;
+        }
+    </style>
+    <script>
+        function closeGame(){
+window.location.href='https://88xgames.com/'; }
+window.gclose=closeGame;
+window.close=closeGame;
+window.onmessage=function(event){
+if (event.data=='closeGame' || event.data=='close' || event.data.indexOf("GAME_MODE:LOBBY")>=0) {
+closeGame(); } }
+    </script>
+</head>
+
+<body>
+    <iframe id="frame"
+        src="{{ $url }}"
+        allowfullscreen></iframe>
+</body>
+
+</html>
