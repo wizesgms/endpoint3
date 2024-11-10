ab25422f3a5180c2d352b9114822dc0fd4b7ac1a
wizesgms <161136541+wizesgms@users.noreply.github.com>
2024-11-11 01:57:17 +0700

    Update ApiController.php

--- core/app/Http/Controllers/Api/ApiController.php
+++ core/app/Http/Controllers/Api/ApiController.php
@@ -150,10 +150,10 @@ class ApiController extends Controller
         }
 
         $balance = $this->api_balance($data['user_code']);
-        $data = $balance['data'];
+        $datas = $balance['data'];
 
         DB::table('users')->where('userCode', $data['user_code'])->update([
-            'balance' => $data['balance'],
+            'balance' => $datas['balance'],
         ]);
 
         $player = DB::table('users')->where('userCode', $data['user_code'])->where('agentCode', $data['agent_code'])->first();
