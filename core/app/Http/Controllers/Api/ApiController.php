640337bba8dda7020e5c7e64e03d63a10f9b9e0c
wizesgms <161136541+wizesgms@users.noreply.github.com>
2024-11-14 02:12:53 +0700

    Update ApiController.php

--- core/app/Http/Controllers/Api/ApiController.php
+++ core/app/Http/Controllers/Api/ApiController.php
@@ -533,10 +533,11 @@ class ApiController extends Controller
     public function provider_save(Request $request)
     {
         $provider = DB::table('game_lists')->where('ProviderCode',$request->provider)->get(['Provider', 'GameCode', 'GameName','GameType', 'ProviderCode','GameImage','Status']);
-        $count = DB::table('game_lists')->count();
+        $count = DB::table('game_lists')->where('ProviderCode',$request->provider)->count();
         return response()->json([
             'ProviderGames' => $provider,
             'ErrorCode' => 0,
+            'total' => $count,
             'ErrorMessage' => 'SUCCESS'
         ], 200);
     }
