d70ef11cdbe39cbb421f64791459fd1ad0f63639
wizesgms <161136541+wizesgms@users.noreply.github.com>
2024-11-30 20:17:47 +0700

    Update iframe.blade.php

--- core/resources/views/iframe.blade.php
+++ core/resources/views/iframe.blade.php
@@ -1,37 +1,88 @@
+<!DOCTYPE html>
 <html>
-
 <head>
     <title>{{ $games->game_name }}</title>
-    <style type="text/css">
-        html,
-        body,
-        iframe {
-            left: 0px;
-            top: 0px;
-            position: relative;
-            display: flex;
-            align-items: center;
-            background-color: black;
-            margin: 0px;
-            padding: 0px;
-            width: 100%;
-            height: 100%;
-            border: 0px;
-        }
-    </style>
-    <script>
-        function closeGame(){
-window.location.href='/'; }
-window.gclose=closeGame;
-window.close=closeGame;
-window.onmessage=function(event){
-if (event.data=='closeGame' || event.data=='close' || event.data.indexOf("GAME_MODE:LOBBY")>=0) {
-closeGame(); } }
-    </script>
-</head>
+    <meta charset="utf-8">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui">
+      <style>
+         body,
+         html {
+         position: fixed;
+         } 
+      </style>
+   </head>
 
-<body>
-    <iframe id="frame" src="{{ $games->url }}" allowfullscreen></iframe>
+
+
+<body style="margin:0px;width:100%;background-color:black;overflow:hidden">
+
+
+
+<iframe id='game' style="position:absolute;top:0px;margin:0px;border:0px;width:100%;height:100vh;" src='{{ $games->url }}' allowfullscreen>
+
+
+</iframe>
+
+
+
+
 </body>
+
+
+<script>
+
+var exitUrl='';
+if(document.location.href.split("api_exit=")[1]!=undefined){
+		exitUrl=document.location.href.split("api_exit=")[1].split("&")[0];
+		}
+addEventListener('message',function(ev){
+	
+if(ev.data=='CloseGame'){
+var isFramed = false;
+try {
+	isFramed = window != window.top || document != top.document || self.location != top.location;
+} catch (e) {
+	isFramed = true;
+}
+
+if(isFramed ){
+window.parent.postMessage('CloseGame',"*");	
+}
+document.location.href=exitUrl;	
+}
+	
+	});
+	
+var gm=document.getElementById("game");			
+	
+function	FormatViewport(){
+	
+
+	
+gm.style['height']=window.innerHeight+'px';	
+gm.style['top']=window.scrollY+'px';	
+	
+}
+	
+	
+window.onresize=FormatViewport;	
+
+setInterval(function(){
+	
+
+	
+
+
+FormatViewport();		
+	
+	
+},500);
+
+FormatViewport();	
+	
+	
+</script>
 
 </html>
